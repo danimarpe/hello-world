1b5cc95268be1833d7f2252f817919ba0bfd3589
Dani <Dani@DESKTOP-U5UD04K>
2016-10-18 12:11:52 +0200

    Entities created

---
+++ DuplicatesDetection/DuplicatesDetection/Entities/AddressEntity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicatesDetection.Entities
+{
+    public class AddressEntity
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip_Code { get; set; }
+
+        public override bool Equals(object addressObject)
+        {
+            AddressEntity address = (AddressEntity)addressObject;
+
+            if (this.Street.Equals(address.Street) &&
+                this.City.Equals(address.City) &&
+                this.State.Equals(address.State) &&
+                this.Zip_Code.Equals(address.Zip_Code))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 23 + 31 * Street.GetHashCode() + City.GetHashCode() + State.GetHashCode() + Zip_Code.GetHashCode();
+
+            return hash / 4;
+        }
+    }
+}
---
+++ DuplicatesDetection/DuplicatesDetection/Entities/UserEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicatesDetection.Entities
+{
+    public class UserEntity
+    {
+        public int Order_ID { get; set; }
+        public int Deal_ID { get; set; }
+        public string Email { get; set; }
+        public string Credit_Card { get; set; }
+        public AddressEntity Address { get; set; }
+    }
+}
